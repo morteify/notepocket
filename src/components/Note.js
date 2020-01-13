383baf9ba0782f3dd31e374622189f0e5ac0010e
Michał Węglarz <michal.weglarz@protonmail.com>
2020-01-13 02:56:31 +0100

    Add basic option to save title and content of a note

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,7 +1,46 @@
-class Note {
-  constructor() {
-    this.title = '';
-    this.content = '';
+export class Note {
+  constructor(state, initialValues) {
+    this.form = {};
+    this.title = initialValues ? initialValues.title : '';
+    this.content = initialValues ? initialValues.content : '';
+    this.date = null;
     this.tags = [];
+    this.state = state;
   }
+
+  addTitle = () => {
+    const text = document.querySelector('#noteTitleTextarea');
+    text.addEventListener('change', () => {
+      this.title = text.value;
+    });
+  };
+
+  addContent = () => {
+    const text = document.querySelector('#noteContentTextarea');
+    text.addEventListener('change', () => {
+      this.content = text.value;
+    });
+  };
+
+  addDate = () => {
+    this.date = Date.now();
+  };
+
+  saveNote = () => {
+    const saveButton = document.querySelector('#save-note');
+    saveButton.addEventListener('click', () => {
+      this.addDate();
+      this.state.push({
+        title: this.title,
+        content: this.content,
+        date: this.date,
+      });
+    });
+  };
+
+  render = () => {
+    this.addContent();
+    this.addTitle();
+    this.saveNote();
+  };
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
-const note = {
-  title: '',
-  content: '',
-  tags: [],
-};
+import { Note } from './components/Note.js';
 
-const state = {
-  savedNote: '',
-};
+const noteExcerpt = document.querySelector('#noteExcerpt');
+const noteContent = document.querySelector('#noteContent');
+let savedNotes = [];
+
+const savedNotesProxy = new Proxy(savedNotes, {
+  set: (target, key, value) => {
+    target[key] = value;
+    console.log('state', savedNotes);
+    if (typeof value !== 'number') updateExcerpt(value);
+    return true;
+  },
+});
+
+function updateExcerpt(note) {
+  const node = document.createElement('p');
+  node.innerText += note.title;
+  node.innerText += note.date;
+  noteExcerpt.appendChild(node);
+}
+
+const noteView = new Note(savedNotesProxy);
+noteView.render();
